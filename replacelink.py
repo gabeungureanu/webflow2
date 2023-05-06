00c2772c26588d6db129ea88cf61530fb13f75d0
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-05-06 10:05:53 +0530

    Update replacelink.py

--- replacelink.py
+++ replacelink.py
@@ -1,15 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
-import os
-import base64
 import json
-import re
+
 
 
 url = 'https://api.github.com/repos/gabeungureanu/webflow2/contents/index.html'
 headers = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization": "Bearer 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"
+    "Authorization": "Bearer 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"
 }
 
 response = requests.get(url, headers=headers)
