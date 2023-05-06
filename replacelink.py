b1733290e84658e66ea8e4629b64170690db73ce
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-05-06 10:03:48 +0530

    Update replacelink.py

--- replacelink.py
+++ replacelink.py
@@ -9,31 +9,9 @@ import re
 url = 'https://api.github.com/repos/gabeungureanu/webflow2/contents/index.html'
 headers = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization": "Bearer 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"
+    "Authorization": "Bearer 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"
 }
 
 response = requests.get(url, headers=headers)
 data = json.loads(response.content)
 
-content = base64.b64decode(data['content']).decode('utf-8')
-#content = content.replace("old_content", "new_content")
-content = content.replace("https://uploads-ssl.webflow.com/63ff41bea75a04f76f8bbd7a", "images")
-content = content.replace("https://uploads-ssl.webflow.com/63ff41bea75a049e418bbd55", "images")
-#content = content.replace("https://uploads-ssl.webflow.com/63ff41bea75a049e418bbd55", "images/")
-
-
-new_content = base64.b64encode(content.encode('utf-8')).decode('utf-8')
-data = {
-    "message": "Update file",
-    "content": new_content,
-    "sha": data['sha']
-}
-
-response = requests.put(url, headers=headers, json=data)
-
-
-
-if response.status_code == 200:
-    print('File updated successfully!')
-else:
-    print('An error occurred while updating the file:', response.content)
