e829fd7ea573283616c9cd7a64e590a2f8d1f7fb
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-05-06 10:07:50 +0530

    Update replacelink.py

--- replacelink.py
+++ replacelink.py
@@ -6,9 +6,9 @@ import json
 url = 'https://api.github.com/repos/gabeungureanu/webflow2/contents/index.html'
 headers = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization": "Bearer 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"
+    "Authorization": "Bearer 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"
 }
 
 response = requests.get(url, headers=headers)
-data = json.loads(response.content)
+
 
