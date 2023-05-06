65d6dcfe432db7f1ca4db6604a7a20c244b23f2f
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-05-06 10:24:35 +0530

    Update replacelink.py

--- replacelink.py
+++ replacelink.py
@@ -1,6 +1,31 @@
 import requests
+from bs4 import BeautifulSoup
+import base64
 import json
 
 
- print('File updated successfully!')
 
+url = 'https://api.github.com/repos/gabeungureanu/webflow2/contents/index.html'
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": "Bearer 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"
+}
+
+response = requests.get(url, headers=headers)
+data = json.loads(response.content)
+
+content = base64.b64decode(data['content']).decode('utf-8')
+#content = content.replace("old_content", "new_content")
+content = content.replace("https://uploads-ssl.webflow.com/63ff41bea75a04f76f8bbd7a", "images")
+content = content.replace("https://uploads-ssl.webflow.com/63ff41bea75a049e418bbd55", "images")
+#content = content.replace("https://uploads-ssl.webflow.com/63ff41bea75a049e418bbd55", "images/")
+
+
+new_content = base64.b64encode(content.encode('utf-8')).decode('utf-8')
+data = {
+    "message": "Update file",
+    "content": new_content,
+    "sha": data['sha']
+}
+
+response = requests.put(url, headers=headers, json=data)
