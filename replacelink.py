3a44ddf7528c2b3b7f0e92e778fa30e5675aab1c
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-05-08 19:01:51 +0530

    Update replacelink.py

--- replacelink.py
+++ replacelink.py
@@ -1,8 +1,9 @@
-
+import requests
+import base64
 
 # Set up authentication credentials
 user = 'gabeungureanu'
-token = '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'
+token = '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'
 headers = {'Authorization': f'token {token}'}
 
 
@@ -12,12 +13,32 @@ repo_name = 'webflow2'
 file_path = 'index.html'
 
 
-# Make the API request to retrieve the file contents
+# Define the new content for the file
+new_content = 'This is the new content of the file'
+
+# Make an API request to get the current content of the file
 url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}'
 response = requests.get(url, headers=headers)
 
-# Extract the file content from the API response
-file_content = response.json()['content']
+# Get the SHA of the file for the update request
+sha = response.json()['sha']
 
-# Decode the base64-encoded file content
-content = base64.b64decode(file_content).decode('utf-8')
+# Decode the content of the file
+content = base64.b64decode(response.json()['content']).decode('utf-8')
+
+# Replace the content with the new content
+updated_content = content.replace(content, new_content)
+
+# Encode the updated content in Base64 format
+encoded_content = base64.b64encode(updated_content.encode('utf-8')).decode('utf-8')
+
+# Define the API endpoint and payload for updating the file
+api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}'
+payload = {
+    'message': 'Update file content',
+    'content': encoded_content,
+    'sha': sha
+}
+
+# Make an API request to update the file content
+response = requests.put(api_url, headers=headers, json=payload)
