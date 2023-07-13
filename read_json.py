c7228e0974f997d5dadd902cf9081f930ea9e4c3
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-13 18:11:27 +0530

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -4,11 +4,11 @@ file_path = '/home/runner/work/webflow2/webflow2/recipients.json'  # Replace wit
 
 try:
     with open(file_path, 'r') as json_file:
-        data = json.load(json_file)
+        json_data = json.load(json_file)
         recipients = data['recipients']
-        concatenated = ', '.join(recipient['email'] for recipient in recipients)
+       # concatenated = ', '.join(recipient['email'] for recipient in recipients)
        # print(concatenated)
-      print(f"::set-output name=data::{data}")
+      print(f"::set-output name=data::{json_data}")
 except Exception as e:
     print(f"Error reading JSON file: {e}")
     exit(1)
