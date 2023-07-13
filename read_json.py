c8cc13128323769a838b48092048609c2180c6b2
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-13 15:04:17 +0530

    Create read_json.py

---
+++ read_json.py
@@ -0,0 +1,11 @@
+import json
+
+file_path = './webflow2/recipients.json'  # Replace with the actual path to your JSON file
+
+try:
+    with open(file_path, 'r') as json_file:
+        json_data = json.load(json_file)
+        print(json_data)
+except Exception as e:
+    print(f"Error reading JSON file: {e}")
+    exit(1)
