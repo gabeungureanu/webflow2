63bdc41b43a42be8f5c94addbe3933ecb251a06f
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-13 18:01:21 +0530

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -7,7 +7,8 @@ try:
         data = json.load(json_file)
         recipients = data['recipients']
         concatenated = ', '.join(recipient['email'] for recipient in recipients)
-        print(concatenated)        
+       # print(concatenated)
+      print(f"::set-output name=data::{concatenated}")
 except Exception as e:
     print(f"Error reading JSON file: {e}")
     exit(1)
