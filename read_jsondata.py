9af47f51a34ef922c0c844bf11546f663d5f3e04
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-13 19:27:47 +0530

    Update read_jsondata.py

--- read_jsondata.py
+++ read_jsondata.py
@@ -1,5 +1,5 @@
 import json
-
+file_path = '/home/runner/work/webflow2/webflow2/recipients.json'
 def read_json_file(file_path):
     with open(file_path) as file:
        data = json.load(json_file)
