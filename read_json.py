42132ad1daf1b03b7a036dd8e94a5adba015ab33
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-13 17:04:39 +0530

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -5,7 +5,7 @@ file_path = '/home/runner/work/webflow2/webflow2/recipients.json'  # Replace wit
 try:
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
-        print(data)
+        print(data.recipients)
 except Exception as e:
     print(f"Error reading JSON file: {e}")
     exit(1)
