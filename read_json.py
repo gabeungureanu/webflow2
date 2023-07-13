2d440db340c9e2ba03fe368461083d91f0b7d19b
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-13 17:13:59 +0530

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -5,8 +5,9 @@ file_path = '/home/runner/work/webflow2/webflow2/recipients.json'  # Replace wit
 try:
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
-        innerdata = json.load(data)
-        print(innerdata)
+        recipients = data['recipients']
+        concatenated = ', '.join(recipient['email'] for recipient in recipients)
+        print(concatenated)
 except Exception as e:
     print(f"Error reading JSON file: {e}")
     exit(1)
