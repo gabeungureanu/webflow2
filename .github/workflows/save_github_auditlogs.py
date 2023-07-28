31beb643adc7c1ed2f64ac77c2f243258146af3e
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-28 13:53:28 +0530

    Update save_github_auditlogs.py

--- .github/workflows/save_github_auditlogs.py
+++ .github/workflows/save_github_auditlogs.py
@@ -12,7 +12,7 @@ import os
 #        return []
 
 def add_data_to_audit_log(new_data):
-    file_path = "python/audit_log.json"
+    file_path = "audit_log.json"
     # Step 1: Read the existing JSON data from the file (if it exists)
     try:
         if os.path.exists(file_path):
