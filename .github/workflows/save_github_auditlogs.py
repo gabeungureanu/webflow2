a505e8c4e80e8ea5ad33bf9c5412725a7e3b08ec
gabeungureanu <129301812+gabeungureanu@users.noreply.github.com>
2023-07-28 13:46:39 +0530

    Update save_github_auditlogs.py

--- .github/workflows/save_github_auditlogs.py
+++ .github/workflows/save_github_auditlogs.py
@@ -1,15 +1,15 @@
 import json
 import os
 
-def get_pushed_files():
-    try:
-        event_path = os.environ["GITHUB_EVENT_PATH"]
-        with open(event_path, 'r') as event_file:
-            event_data = json.load(event_file)
-        return event_data["commits"][0]["added"] + event_data["commits"][0]["modified"] + event_data["commits"][0]["removed"]
-    except Exception as e:
-        print(f"Failed to get pushed files: {e}")
-        return []
+#def get_pushed_files():
+#    try:
+#        event_path = os.environ["GITHUB_EVENT_PATH"]
+#        with open(event_path, 'r') as event_file:
+#            event_data = json.load(event_file)
+#        return event_data["commits"][0]["added"] + event_data["commits"][0]["modified"] + event_data["commits"][0]["removed"]
+#    except Exception as e:
+#        print(f"Failed to get pushed files: {e}")
+#        return []
 
 def add_data_to_audit_log(new_data):
     file_path = "python/audit_log.json"
@@ -33,14 +33,14 @@ def add_data_to_audit_log(new_data):
 
 if __name__ == "__main__":
     # Get the list of files pushed in the event
-    pushed_files = get_pushed_files()
+    #pushed_files = get_pushed_files()
 
     # Sample new data to add to the audit log
     new_data = {
         "event": "new_event",
         "timestamp": "2023-07-27T12:34:56Z",
         "details": "Some details about the new event",
-        "files": pushed_files
+        #"files": pushed_files
     }
 
     add_data_to_audit_log(new_data)
